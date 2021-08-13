3675ced720a2fe77203f41d6b99f6b88714a2998
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-14 01:38:53 +0200

    updated positioning of text in label

--- SFML_RPG_Game/src/ui_objects/Label.cpp
+++ SFML_RPG_Game/src/ui_objects/Label.cpp
@@ -10,10 +10,15 @@ Label::Label(const sf::Font& font, const std::string& text, const sf::Vector2f& 
 	m_text.setOutlineColor(sf::Color::Black);
 	m_text.setOutlineThickness(2.f);
 
-	m_text.setOrigin(m_text.getLocalBounds().left + m_text.getLocalBounds().width / 2.0f,
-		m_text.getLocalBounds().top + m_text.getLocalBounds().height / 2.0f);
 
-	m_text.setPosition(sf::Vector2f(constants::SCR_WIDTH / 2.0f, position.y));
+	//m_text.setOrigin(m_text.getLocalBounds().left + m_text.getLocalBounds().width / 2.0f,
+	//	m_text.getLocalBounds().top + m_text.getLocalBounds().height / 2.0f);
+	//m_text.setOrigin(m_text.getLocalBounds().left / 2.f, m_text.getLocalBounds().top / 2.f);
+
+	m_text.setPosition(position);
+	m_text.setOrigin(size.x / 2.f, size.y / 2.f);
+	
+	//m_text.setPosition(sf::Vector2f(constants::SCR_WIDTH / 2.0f, position.y));
 }
 
 
