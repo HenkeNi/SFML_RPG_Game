e723c93b435df392b8d42c2510dfb7b09b0507ef
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-14 01:43:23 +0200

    changed the position sent to uiobject
    also implemented on Settings button pressed logic

--- SFML_RPG_Game/src/states/MainMenuState.cpp
+++ SFML_RPG_Game/src/states/MainMenuState.cpp
@@ -1,4 +1,5 @@
 #include "MainMenuState.h"
+#include "SettingsState.h"
 #include "../ui_objects/Button.h"
 #include "../ui_objects/Label.h"
 
@@ -14,24 +15,16 @@ MainMenuState::MainMenuState(SharedContext context, KeyBinding& keyBindings, Sta
 
 void MainMenuState::draw()
 {
-	//m_windowContext.draw(m_background); // TODO: FIX ?? - maybe pass in RenderTarget instead?
 	m_context.m_windowPtr->draw(m_background);
 
 	for (auto itr{ m_uiComponents.begin() }; itr != m_uiComponents.end(); ++itr)
 		m_context.m_windowPtr->draw(*itr->second);
-
-	//for (const auto& c : m_uiComponents)
-	//	m_windowContext.draw(*c);
-
 }
 
 
 bool MainMenuState::update(sf::Time dt)
 {
 	updateMousePositions();
-
-	std::cout << "Mouse pos: " << m_mousePosView.x << ", " << m_mousePosView.y << '\n';
-
 
 	//for (const auto& comp : m_uiComponents)
 	//	comp->update(m_mousePosView);
@@ -57,12 +50,18 @@ bool MainMenuState::update(sf::Time dt)
 
 
 
-	// FIX: put in function or handle differently (FIX THIS!!!!)
+	// FIX: put in function or handle differently (FIX THIS!!!!) PUT IN HANDLEEVENT???
 	if (m_uiComponents["START_GAME"]->isSelected()) // TOOD: start game....
 	{
 		requestStackPop();
 		return false;
 	}
+
+
+
+	if (m_uiComponents["SETTINGS"]->isSelected())
+		requestStackPush(std::make_unique<SettingsState>(SharedContext{ m_context.m_windowPtr,  m_context.m_texturesPtr, m_context.m_fontsPtr }, m_keyBindingContext, m_stackContext));
+
 
 	if (m_uiComponents["QUIT"]->isSelected())
 		; // Request clear stack??
@@ -89,31 +88,35 @@ bool MainMenuState::handleEvent(const sf::Event& event)
 
 
 void MainMenuState::initUIComponents()
-{
+{	
+	auto windowCenterX{ m_context.m_windowPtr->getSize().x / 2.f };
+	auto windowCenterY{ m_context.m_windowPtr->getSize().y / 2.f };
+
+	float buttonWidth{ 400.f };
+	float buttonHeight{ 65.f };
+
+	// Background
 	m_background.setTexture(m_context.m_texturesPtr->getResource("main_menu_background"));
 	m_background.setScale({ 1.5f, 1.5f });
 	
-	//m_background.setSize(static_cast<sf::Vector2f>(m_windowContext.getSize()));
-	
 
-	sf::Vector2f playPosition{ static_cast<float>(m_context.m_windowPtr->getPosition().x - 75.f), static_cast<float>(m_context.m_windowPtr->getPosition().y + 150) };
-	Button playBtn{ m_context.m_fontsPtr->getResource("main-font"), "START GAME", playPosition, { 400.f, 70.f} };
-	//button.setPosition({ 300.0f, 200.0f });
-	playBtn.setScale(2.0f, 2.0f);
-
-	//m_uiComponents["PlayBtn"] = button;
-	//m_uiComponents.insert(std::pair<std::string, std::unique_ptr<Button>>("PLAY", std::make_unique<Button>(playBtn)));
-	m_uiComponents.insert(std::make_pair("START_GAME", std::make_unique<Button>(playBtn)));
-	//m_uiComponents.push_back(std::make_unique<Button>(button)); // TODO: make stack instead??
-
-
-
-	sf::Vector2f quitPosition{ 400, 400 };
-	Button quitBtn{ m_context.m_fontsPtr->getResource("main-font"), "QUIT GAME", quitPosition, { 400.f, 70.f} };
-	m_uiComponents.insert(std::make_pair("QUIT", std::make_unique<Button>(quitBtn)));
-	//m_uiComponents.insert(std::pair<std::string, std::unique_ptr<Button>>("QuitBtn", std::make_unique<Button>(quitBtn)));
-
-
-	Label title{ m_context.m_fontsPtr->getResource("main-font"), "SFML RPG Game", sf::Vector2f{0.f, 80.f}, { 400.f, 70.f } };
+	// Title
+	Label title{ m_context.m_fontsPtr->getResource("main-font"), "SFML RPG Game", { windowCenterX, 100.f }, { 400.f, 65.f } };
 	m_uiComponents.insert(std::make_pair("TITLE", std::make_unique<Label>(title)));
+
+
+	// Start Game Button
+	Button playBtn{ m_context.m_fontsPtr->getResource("main-font"), "START GAME", { windowCenterX, 225.f }, { buttonWidth, buttonHeight } };
+	m_uiComponents.insert(std::make_pair("START_GAME", std::make_unique<Button>(playBtn)));
+
+
+	// Settings button
+	Button settingsButton{ m_context.m_fontsPtr->getResource("main-font"), "SETTINGS", { windowCenterX, 325.f }, { buttonWidth, buttonHeight } };
+	m_uiComponents.insert(std::make_pair("SETTINGS", std::make_unique<Button>(settingsButton)));
+
+	// Quit Button
+	Button quitBtn{ m_context.m_fontsPtr->getResource("main-font"), "QUIT GAME", { windowCenterX, 425.f}, { buttonWidth, buttonHeight } };
+	m_uiComponents.insert(std::make_pair("QUIT", std::make_unique<Button>(quitBtn)));
+
+
 }
