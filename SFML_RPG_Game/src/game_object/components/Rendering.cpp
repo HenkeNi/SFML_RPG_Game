5bf576f8865b5e0350cfca6e0be3830b0abb5157
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-14 01:45:32 +0200

    constructor now accepting start position for sprite

--- SFML_RPG_Game/src/game_object/components/Rendering.cpp
+++ SFML_RPG_Game/src/game_object/components/Rendering.cpp
@@ -1,9 +1,11 @@
 #include "Rendering.h"
 
 
-Rendering::Rendering(const sf::Texture& texture)
-	: m_sprite{ texture }
+
+Rendering::Rendering(const sf::Texture& texture, sf::RenderWindow& target, const sf::Vector2f& startPos)
+	: m_sprite{ texture }, m_target{ target }
 {
+	m_sprite.setPosition(startPos);
 }
 
 
@@ -13,7 +15,19 @@ void Rendering::draw(sf::RenderTarget& target) const
 }
 
 
+void Rendering::draw() const
+{
+	m_target.draw(m_sprite);
+}
+
+
 void Rendering::update(sf::Time dt)
 {
+	//draw();
+}
 
+
+void Rendering::setTextureRect(const sf::IntRect& rect)
+{
+	m_sprite.setTextureRect(rect);
 }
