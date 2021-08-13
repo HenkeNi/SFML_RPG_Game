6f0f0e0084308e845205a624b0f58a828ba846c3
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-14 01:46:19 +0200

    added Animation component (aggregation)
    also checking direction of movement, and idle

--- SFML_RPG_Game/src/game_object/components/Movement.cpp
+++ SFML_RPG_Game/src/game_object/components/Movement.cpp
@@ -1,35 +1,54 @@
 #include "Movement.h"
-
+#include "Animation.h"
 #include "Rendering.h"
 
+#include <iostream>
 
-Movement::Movement(Rendering* render)
-	: m_renderingComponent{ render }, m_movementSpeed{ 10.f }
+Movement::Movement(Rendering* render, Animation* animation)
+	: m_renderingComponent{ render }, m_animationComponent{ animation }, m_movementSpeed{ 60.f }
 {
 }
 
 
-
-void Movement::setPosition(const sf::Vector2f& pos)
+void Movement::setVelocity(const sf::Vector2f& velocity)
 {
-	m_renderingComponent->getSprite().setPosition(pos);
-}
-
-void Movement::setPosition(float vx, float vy)
-{
-	m_renderingComponent->getSprite().setPosition(vx, vy);
+	m_velocity = velocity;
 }
 
 
-
-void Movement::move(const sf::Vector2f& pos)
+void Movement::setVelocity(float vx, float vy)
 {
-	m_renderingComponent->getSprite().move(pos.x * m_movementSpeed, pos.y * m_movementSpeed);
+	m_velocity.x = vx;
+	m_velocity.y = vy;
 }
 
-void Movement::move(float vx, float vy)
+
+
+void Movement::accelerate(const sf::Vector2f& velocity)
 {
-	m_renderingComponent->getSprite().move(vx * m_movementSpeed, vy * m_movementSpeed);
+	m_velocity += velocity;
+}
+
+
+void Movement::accelerate(float vx, float vy)
+{
+	m_velocity.x += vx;
+	m_velocity.y += vy;
+}
+
+
+void Movement::decelerate(sf::Time dt)
+{
+	if (m_velocity.x > 0.f)
+		m_velocity.x -= (m_movementSpeed * dt.asSeconds());
+	if (m_velocity.x < 0.f)
+		m_velocity.x += (m_movementSpeed * dt.asSeconds());
+
+	
+	if (m_velocity.y > 0.f)
+		m_velocity.y -= (m_movementSpeed * dt.asSeconds());
+	if (m_velocity.y < 0.f)
+		m_velocity.y += (m_movementSpeed * dt.asSeconds());
 }
 
 
@@ -41,5 +60,56 @@ void Movement::setMovementSpeed(float speed)
 
 void Movement::update(sf::Time dt) 
 {
+	if (m_renderingComponent)
+		m_renderingComponent->getSprite().setPosition(m_velocity * dt.asSeconds() * m_movementSpeed);
+
+
+	if (m_animationComponent)
+	{
+		static sf::Vector2f last{ 0.f, 0.f };
+
+		if (last.x > m_velocity.x) // move left?
+			//m_animationComponent->
+			std::cout << "Moving left!\n"; // TODO: just change spritesheet??
+		if (last.x < m_velocity.x) // move right?
+			std::cout << "Moving right\n";
+		if (last.y > m_velocity.y) // move up?
+			std::cout << "Moving up\n";
+		if (last.y < m_velocity.y) // move down?
+			std::cout << "Moving down\n";
+
+
+		if (last.x == m_velocity.x && last.y == m_velocity.y) // check if idle left, right?? - need to check if previous larger or smaller
+			std::cout << "Idle\n";
+
+		last = m_velocity;
+	}
+
+	
+
+
+		//m_renderingComponent->getSprite().move(m_velocity * dt.asSeconds() * m_movementSpeed);
+
+
+
+	//m_velocity.x = 0.f;
+	//m_velocity.y = 0.f;
+
+	//decelerate(dt);
+
+	//decelerate();
+	// TODO:
+
+	// if (velocity.x > 0.f) moving left
+		// set animation to moving left??
+
+
+	// also check if x is larger than y (if moving left and up for example)...
 
 }
+
+
+
+
+
+
--- SFML_RPG_Game/src/game_object/components/Movement.h
+++ SFML_RPG_Game/src/game_object/components/Movement.h
@@ -3,26 +3,45 @@
 
 #include <SFML/Graphics.hpp>
 
+class Animation;
 class Rendering;
 
 class Movement : public Component
 {
 public:
-	Movement(Rendering* render);
+	Movement(Rendering* render, Animation* animation = nullptr);
 	
-	void setPosition(const sf::Vector2f& pos);
-	void setPosition(float vx, float vy);
 
-	void move(const sf::Vector2f& pos);
-	void move(float vx, float vy);
+	// TODO: rename setPosition and Move??
+	void setVelocity(const sf::Vector2f& velocity);
+	void setVelocity(float vx, float vy);
+
+
+	void accelerate(const sf::Vector2f& velocity);
+	void accelerate(float vx, float vy);
+
+	void decelerate(sf::Time dt);
+
+	//void setPosition(const sf::Vector2f& pos); // sprite.move is shortcut for setPosition??
+	//void setPosition(float vx, float vy);
+
+	//void move(const sf::Vector2f& pos);
+	//void move(float vx, float vy);
+
+
+	// FOR TEST PURPOSE
+	inline sf::Vector2f getPosition() const { return m_velocity; }
+
 
 	void setMovementSpeed(float speed);
 
 	virtual void update(sf::Time dt) override;
 
 private:
-	Rendering*		 m_renderingComponent;
-	sf::Vector2f m_velocity;
-	float		 m_movementSpeed;
+	Animation*		m_animationComponent;
+
+	Rendering*		m_renderingComponent; // TODO: use friend class instead??
+	sf::Vector2f	m_velocity;
+	float			m_movementSpeed;
 };
 
