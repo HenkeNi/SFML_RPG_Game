035cac0fff78a0ff58aac70ca70d9498fb66644c
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-14 01:45:11 +0200

    added a sf::RenderWindow reference (aggregation)

--- SFML_RPG_Game/src/game_object/components/Rendering.h
+++ SFML_RPG_Game/src/game_object/components/Rendering.h
@@ -1,21 +1,26 @@
 #pragma once
 
 #include "Component.h"
-
 /*
 *	TODO: instead of Movement having a ref to a Render component, let Render have a ref to movement comp (before draw -> check or move??)
 */
 class Rendering : public Component
 {
 public:
-	Rendering(const sf::Texture& texture);
+	Rendering(const sf::Texture& texture, sf::RenderWindow& target, const sf::Vector2f& startPos);
 
 	void draw(sf::RenderTarget& target) const;
+	void draw() const;
 	virtual void update(sf::Time dt) override;
 
-	inline sf::Sprite& getSprite() { return m_sprite; }
+	inline sf::Sprite&   getSprite() { return m_sprite; }
+	inline sf::FloatRect getSize()   { return m_sprite.getLocalBounds(); }
+
+	void setTextureRect(const sf::IntRect& rect);
 
 private:
 	sf::Sprite m_sprite;
+	sf::RenderWindow& m_target;
+	//unsigned int m_frameCount; -- store number of frames in sprite here?? - maybe bad idea!
 };
 
