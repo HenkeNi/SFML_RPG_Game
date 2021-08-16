943a9b39f58514d55d392b9bd27e9a2914c6f6ca
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-16 18:37:29 +0200

    function for returning the drawn part of the sprite  (intRect)

--- SFML_RPG_Game/src/game_object/components/Rendering.cpp
+++ SFML_RPG_Game/src/game_object/components/Rendering.cpp
@@ -1,11 +1,13 @@
 #include "Rendering.h"
 
-
+#include "Collider.h"
 
 Rendering::Rendering(const sf::Texture& texture, sf::RenderWindow& target, const sf::Vector2f& startPos)
 	: m_sprite{ texture }, m_target{ target }
 {
+	//m_sprite.setOrigin(sf::Vector2f{ 125.f / 2.f, 140.f / 2.f }); // FIX!!
 	m_sprite.setPosition(startPos);
+
 }
 
 
@@ -18,16 +20,44 @@ void Rendering::draw(sf::RenderTarget& target) const
 void Rendering::draw() const
 {
 	m_target.draw(m_sprite);
+
+	//// TEST::
+	//for (auto itr = m_drawables.begin(); itr != m_drawables.end(); ++itr)
+	//	m_target.draw(itr);
+
+	//m_target.draw(*m_shape);
+	//m_target.draw()
 }
+
+
+void Rendering::draw(sf::Drawable& drawable) const
+{
+	m_target.draw(drawable);
+}
+
+
+//void Rendering::addDrawable(const sf::Drawable& drawable)
+//{
+//	m_drawables.push_back(drawable);
+//}
+//
+//void Rendering::addShape(sf::RectangleShape* shape)
+//{
+//	m_shape = shape;
+//}
+
 
 
 void Rendering::update(sf::Time dt)
 {
 	//draw();
+	//m_sprite.rotate(10.f);
 }
 
 
 void Rendering::setTextureRect(const sf::IntRect& rect)
 {
+	m_partDrawn = rect; // TODO: FIX!! 
+
 	m_sprite.setTextureRect(rect);
 }
--- SFML_RPG_Game/src/game_object/components/Rendering.h
+++ SFML_RPG_Game/src/game_object/components/Rendering.h
@@ -1,6 +1,9 @@
 #pragma once
-
 #include "Component.h"
+
+#include <vector>
+
+
 /*
 *	TODO: instead of Movement having a ref to a Render component, let Render have a ref to movement comp (before draw -> check or move??)
 */
@@ -11,18 +14,29 @@ public:
 
 	void draw(sf::RenderTarget& target) const;
 	void draw() const;
+	void draw(sf::Drawable& drawable) const;
+
+	//void addDrawable(const sf::Drawable& drawable);
+	//void addShape(sf::RectangleShape* shape);
+
 	virtual void update(sf::Time dt) override;
 
-	inline sf::Sprite&   getSprite() { return m_sprite; }
-	inline sf::Vector2u  getSize()   { return m_sprite.getTexture()->getSize(); }
-
+	inline sf::Sprite&   getSprite()   { return m_sprite; }
+	inline sf::Vector2u  getSize()     { return m_sprite.getTexture()->getSize(); }
+	inline sf::IntRect  getRectSize() const { return m_partDrawn; }
+	//inline sf::Vector2f  getPosition() { return m_sprite.getPosition(); }
 	//inline sf::FloatRect getSize()   { return m_sprite.getTexture()->getSize(); }
 
 
 	void setTextureRect(const sf::IntRect& rect);
 
 private:
-	sf::Sprite m_sprite;
+	sf::Sprite m_sprite; // MAYBE a drawstack instead??? or a vector of sf::Drawable??
+	//std::vector<sf::Drawable*> m_drawables;
+	//sf::RectangleShape* m_shape;
+
+	sf::IntRect m_partDrawn;
+
 	sf::RenderWindow& m_target;
 	//unsigned int m_frameCount; -- store number of frames in sprite here?? - maybe bad idea!
 };
