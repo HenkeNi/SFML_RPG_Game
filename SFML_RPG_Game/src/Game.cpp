efbb44c9dabe10cbb2d2ffbcb7d9502bf10289e1
Henrik Jangefelt <henke9105@hotmail.com>
2021-08-16 18:38:22 +0200

    loading in more assets at the start of the game

--- SFML_RPG_Game/src/Game.cpp
+++ SFML_RPG_Game/src/Game.cpp
@@ -121,17 +121,17 @@ void Game::loadFonts()
 void Game::loadTextures()
 {
 	m_textures.load("main_menu_background", "assets/images/background.jpg"); // TODO: should be texture?? or image??
-	m_textures.load("player", "assets/textures/pirate.png");
-	
-	//m_textures.load("player_idle", "assets/textures/sheet_idle.png");
-	//m_textures.load("player_sheet", "assets/textures/pirate_animations.png");
 	m_textures.load("player_sheet", "assets/textures/pirate_sheet.png");
-
 	m_textures.load("tree", "assets/textures/tree.png");
 	m_textures.load("tree2", "assets/textures/tree2.png");
 	m_textures.load("bush", "assets/textures/bush.png");
 	m_textures.load("berry_bush", "assets/textures/berry_bush.png");
 	m_textures.load("rock", "assets/textures/rock.png");
+	m_textures.load("chest", "assets/textures/chest.png");
+	m_textures.load("barrel", "assets/textures/barrel.png");
+	m_textures.load("ship_1", "assets/textures/simple_ship.png");
+	m_textures.load("palm_1", "assets/textures/palm_tree.png");
+
 
 
 
